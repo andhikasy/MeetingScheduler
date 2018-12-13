4e4d7fa3668d7f48f79f64885d6a5be548f51cd3
Andhika Saifuddin Yahya <dhikadotcom@gmail.com>
2018-12-13 14:49:35 +0700

    Template Awal

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function store(Request $request)
+    {
+        
+    }
+    
+    public function signin(Request $request)
+    {
+        
+    }
+}
