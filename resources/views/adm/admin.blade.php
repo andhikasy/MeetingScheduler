4c6e366ffd6a988bd8eca921f086b30a1d8216b6
Bagas Yoni <bagasyoni1997@gmail.com>
2018-12-13 19:03:33 +0700

    admin

--- resources/views/adm/admin.blade.php
+++ resources/views/adm/admin.blade.php
@@ -1,5 +1,5 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<link href="{{asset('assets/login/body.css')}}" rel="stylesheet">
+<link href="{{asset('assets/adm/css/body.css')}}" rel="stylesheet">
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 
